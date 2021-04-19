43ffbd3621f62b33d2e91d19768cbbc83c5e4c10
Caroline Souza <loh_jg@hotmail.com>
2021-04-18 21:24:53 -0300

    adicionando driver do bd

--- src/Banco/ConnectionFactory.java
+++ src/Banco/ConnectionFactory.java
@@ -14,7 +14,7 @@ public class ConnectionFactory {
 		String schema = "tees_acompanhamento_tcc";
 		String servidor = "jdbc:mysql://localhost:" + porta + "/" + schema;
 		String usuario = "root";
-		String senha = "123456";
+		String senha = "1234";
 		
 		try {
 			//Class.forName("com.mysql.jdbc.Driver").newInstance();
